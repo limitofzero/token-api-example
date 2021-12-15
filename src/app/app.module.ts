94b8092da4b19c1b716e1c365207eaad8a59e2c7
limitofzero <limitofzero@gmail.com>
2021-12-15 22:01:54 +0300

    feat: configure project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TUI_SANITIZER,TuiDialogModule, TuiNotificationsModule, TuiRootModule } from '@taiga-ui/core';
+import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        TuiRootModule,
+        BrowserAnimationsModule,
+        TuiDialogModule,
+        TuiNotificationsModule,
+    ],
+    providers: [{ provide: TUI_SANITIZER, useClass: NgDompurifySanitizer }],
+    bootstrap: [AppComponent],
 })
 export class AppModule { }
